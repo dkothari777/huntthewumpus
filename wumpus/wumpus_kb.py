14ed051199f06c4c85c532e87165dd0da6d2bdf8
drvela <drvela@email.arizona.edu>
2015-04-06 13:47:12 -0700

    added bounds on axiom_generator_at_location_ssa

--- wumpus/wumpus_kb.py
+++ wumpus/wumpus_kb.py
@@ -537,10 +537,14 @@ def axiom_generator_at_location_ssa(t, x, y, xmin, xmax, ymin, ymax):
 
     clauses = []
     # Forward clauses
-    clauses.append('(({0} & {1} & {2}) <=> {3})'.format(state_loc_str(x,y,t), state_heading_east_str(t), action_forward_str(t), state_loc_str(x+1,y,t+1)))
-    clauses.append('(({0} & {1} & {2}) <=> {3})'.format(state_loc_str(x,y,t), state_heading_west_str(t), action_forward_str(t), state_loc_str(x-1,y,t+1)))
-    clauses.append('(({0} & {1} & {2}) <=> {3})'.format(state_loc_str(x,y,t), state_heading_north_str(t), action_forward_str(t), state_loc_str(x,y+1,t+1)))
-    clauses.append('(({0} & {1} & {2}) <=> {3})'.format(state_loc_str(x,y,t), state_heading_south_str(t), action_forward_str(t), state_loc_str(x,y-1,t+1)))
+    if x + 1 <= xmax:
+        clauses.append('(({0} & {1} & {2}) <=> {3})'.format(state_loc_str(x,y,t), state_heading_east_str(t), action_forward_str(t), state_loc_str(x+1,y,t+1)))
+    if x - 1 >= xmin:
+        clauses.append('(({0} & {1} & {2}) <=> {3})'.format(state_loc_str(x,y,t), state_heading_west_str(t), action_forward_str(t), state_loc_str(x-1,y,t+1)))
+    if y + 1 <= ymax:
+        clauses.append('(({0} & {1} & {2}) <=> {3})'.format(state_loc_str(x,y,t), state_heading_north_str(t), action_forward_str(t), state_loc_str(x,y+1,t+1)))
+    if y - 1 >= ymin:
+        clauses.append('(({0} & {1} & {2}) <=> {3})'.format(state_loc_str(x,y,t), state_heading_south_str(t), action_forward_str(t), state_loc_str(x,y-1,t+1)))
     clauses.append('({0} <=> (({1} | {2} | {3} | {4} | {6}) & {6}))'.format(state_loc_str(x,y,t+1), action_wait_str(t), action_grab_str(t), action_shoot_str(t), action_turn_left_str(t), action_turn_right_str(t), state_loc_str(x,y,t)))
 
     axiom_str = ' | '.join(clauses)
